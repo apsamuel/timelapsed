6c8ceed4adb989aae8f26170073d42c4614bd9f1
aaronpsamuel <aaron.psamuel@gmail.com>
2015-02-06 06:31:20 -0500

    fixed syntax error in case statement - while should be when...

--- lib/timelapseserver.rb
+++ lib/timelapseserver.rb
@@ -18,24 +18,24 @@ module TimeLapseServer
 
   	def receive_data(data)
   		case data
-  			while /config_camera/
+  			when /config_camera/
   				send_data("Configuring Camera!!")
   				@@turret = CamTurret::MotionEngine.new('/dev/servoblaster', 0)
                 		@@cam = CamVision::ImageEngine.new('/dev/video0')
                 		send_data("Configured camera and turret objects!! #{@@cam} #{@@turret}")
-  			while /take_picture/
+  			when /take_picture/
   				send_data("Taking Picture!!")
   				@@cam.takePicture
   				send_data("Picture Taken!!")
-  			while /shoot_video/
+  			when /shoot_video/
   				send_data("Shooting Video!!")
   				@@cam.shootVideo
   				send_data("Video Complete!!")
-  			while /init_time_lapse/
+  			when /init_time_lapse/
   				send_data("Running Time Lapse!!")
   				@@cam.initTimeLapse
   				send_data("Time Lapse Complete!!")
-   			while /demo_camera/
+   			when /demo_camera/
   				send_data("Running Time Lapse!!")
   				@@cam.demoCamera
   				send_data("Time Lapse Complete!!")
